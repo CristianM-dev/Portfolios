0bdc7dbdec816b09a356a2c997fd9e1273796c8f
Cris <Cris@faso.com.br>
2024-07-20 17:35:37 -0300

    First publication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, Renderer2 } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import {  } from 'express';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  viewAtivo:number = Views.SobreMim;
+
+  Views = Views;
   title = 'Portfolios';
+
+  ScrollListener(event:Event){
+    let el = event.target as HTMLElement;
+    let sectionHeight = el.clientHeight - 50;
+    if(el.scrollTop > sectionHeight*2)
+      this.viewAtivo = Views.Portfolios;
+    else if(el.scrollTop > sectionHeight)
+      this.viewAtivo = Views.Experiencia
+    else this.viewAtivo = Views.SobreMim
+  }
+
+  ScrollTo(id:string){
+    let el = document.getElementById(id);
+    if(el){
+      el.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+}
+
+export  enum Views{
+  SobreMim, Experiencia, Portfolios
 }
